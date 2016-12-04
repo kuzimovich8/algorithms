9b7143499f7fe2c670e0095fb5dd15d82aba344b
kuzimovich8 <kuzimovich8@gmail.com>
2016-12-04 17:30:15 +0200

    Find if the inputted number belongs to Fibonacci series

---
+++ Phibonachi.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int In = int.Parse(Console.ReadLine());
+            int Previous;
+            int This = 0;
+            int Next = 1;
+
+            while (true)
+            {
+                if (This <= In)
+                {
+                    if (This == In)
+                    {
+                        Console.WriteLine("YES");
+                        break;
+                    }
+                    else
+                    {
+                        Previous = This;
+                        This = Next;
+                        Next = Previous + This;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                    break;
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
